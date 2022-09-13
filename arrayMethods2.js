d632d10ef87d3aa5f50e81baaa13e9bc3ce59e74
Lauren <lefdoughty@gmail.com>
2022-09-13 12:57:57 -0400

    Update arrayMethods2.js

---
+++ arrayMethods2.js
@@ -0,0 +1,6 @@
+// sum of two arrays... Actually the sum of all their elements. 
+// Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+  }
